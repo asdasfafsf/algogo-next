3e5adf23c54f524bd45f6114e43493f6132fe36d
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-07-12 03:06:45 +0900

    feat: add landing layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,10 @@
 // shadcn/ui components
-export * from "./ui/button";
-export * from "./ui/input";
-export * from "./ui/card";
-export * from "./ui/typography";
+export * from "./ui/Button";
+export * from "./ui/Input";
+export * from "./ui/Card";
+export * from "./ui/Typography";
+export * from "./ui/Logo";
+export * from "./ui/LogoWithText";
+export * from "./ui/FadeInSection";
+export * from "./layout/LandingHeader";
+export * from "./layout/LandingFooter";
