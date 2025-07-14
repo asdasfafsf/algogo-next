31285b76b25330b42e14205aed1d827f639b4803
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-07-14 12:31:54 +0900

    fix: fix api response type

--- src/lib/api/problem.api.ts
+++ src/lib/api/problem.api.ts
@@ -3,6 +3,6 @@ import { IquiryProblemsSummary, ProblemSummaryList } from "@/types/problem.type"
 import apiClient from "./api-client";
 
 export const getProblemList = async (params: IquiryProblemsSummary) => {
-    const response = await apiClient.post<ApiResponse<ProblemSummaryList[]>>('api/v2/problems', params);
+    const response = await apiClient.post<ApiResponse<ProblemSummaryList>>('api/v2/problems', params);
     return response.data;
 };
