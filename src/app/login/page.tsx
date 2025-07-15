9bda6822f3b142fa3401b2713240c69e9367d241
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-07-15 16:42:39 +0900

    feat: 로딩 페이지 분리, 필요한곳에 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,10 @@
 import AuthPage from '@/components/auth/AuthPage';
 import { Suspense } from 'react';
+import LoadingScreen from '@/components/ui/LoadingScreen';
 
 export default function LoginPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingScreen title="로그인 페이지 로딩 중" description="페이지를 준비하고 있습니다" loadingMessage="Loading" />}>
       <AuthPage />
     </Suspense>
   );
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,10 @@
 import AuthPage from '@/components/auth/AuthPage';
 import { Suspense } from 'react';
+import LoadingScreen from '@/components/ui/LoadingScreen';
 
 export default function SignupPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingScreen title="회원가입 페이지 로딩 중" description="페이지를 준비하고 있습니다" loadingMessage="Loading" />}>
       <AuthPage />
     </Suspense>
   );
