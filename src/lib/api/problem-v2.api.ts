aef5c796c7b2932940a38042936ff16c724875f2
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-07-15 23:25:26 +0900

    feat: add problem api

---
+++ src/lib/api/problem-v2.api.ts
@@ -0,0 +1,27 @@
+import { AxiosResponse } from 'axios';
+import qs from 'qs';
+import { apiClient } from './api-client';
+import {
+  IquiryProblemsSummary, Problem, ProblemSummaryList, TodayProblem,
+} from '@/types/problem.type';
+import { ApiResponse } from '@/types/api.type';
+
+export const getProblemList = async (requestProblemListDto: IquiryProblemsSummary) => {
+  const queryString = qs.stringify(requestProblemListDto, { arrayFormat: 'brackets' });
+  const response: AxiosResponse<ApiResponse<ProblemSummaryList>> = await apiClient.get(`/api/v2/problems?${queryString}`);
+  const problemList = response.data;
+  return problemList;
+};
+
+export const getProblem = async (problemUuid: string): Promise<ApiResponse<Problem>> => {
+  const response: AxiosResponse<ApiResponse<Problem>> = await apiClient.get(`/api/v2/problems/${problemUuid}`);
+  const problem = response.data;
+  return problem;
+};
+
+export const getTodayProblems = async (day: number = 0): Promise<ApiResponse<TodayProblem[]>> => {
+  const url = `/api/v2/problems/today?day=${day}`;
+  const response: AxiosResponse<ApiResponse<TodayProblem[]>> = await apiClient.get(url);
+  const problem = response.data;
+  return problem;
+};
